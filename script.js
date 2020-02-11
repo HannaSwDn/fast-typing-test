1e5c8622f19c5c36449f662fa24f78c0eb947e65
hanna_nyman <hannanyman.sk@gmail.com>
2020-02-12 00:11:15 +0100

    Basic test structure of the test

---
+++ script.js
@@ -0,0 +1,74 @@
+// random words will be generated
+
+let test = {
+    words: [
+        // 2 letters
+        'go',
+        'ex',
+        'be',
+        'no',
+        // 3 letters
+        'fun',
+        'luc',
+        'ham',
+        'dip',
+        // 4 letters
+        'tree',
+        'cute',
+        'lake',
+        'ikea',
+        // 5 letters
+        'hanna',
+        'crowd',
+        'music',
+        'bread',
+        // 6 letters
+        'potato',
+        'sweden',
+        'coffee',
+        'cheese',
+        // 7 letters
+        'ability',
+        'airport',
+        'bullion',
+        'cookies',
+        // 8 letters
+        'licorice',
+        'cinnamon',
+        'beverage',
+        'handsome',
+        // 9 letters
+        'stockholm',
+        'meatballs',
+        'midsummer',
+        'raspberry',
+    ],
+
+    generateWord() {
+        // generate a new word to the array of words
+    },
+
+    analyzeKeyPress() {
+        // compares what letter user press to current letter of current word
+    },
+
+    wrongLetter() {
+        // if user types incorrectly, make current letter red
+    },
+
+    rightLetter() {
+        // if user types correctly, make current letter blue
+    },
+
+    skipWord() {
+        // user can skip a word by clicking the spacebar
+    },
+
+    startTimer() {
+        // starting the 1 minute timer
+    },
+
+    stopTimer() {
+        // stopping the 1 minute timer
+    }
+}
